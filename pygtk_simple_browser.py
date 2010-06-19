031edee584cd122c2ad5a206416cf960f8d624b8
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-06-19 05:13:02 +0200

    * major rewrite, using tabs

--- pygtk_simple_browser.py
+++ pygtk_simple_browser.py
@@ -12,87 +12,175 @@
 import gtk
 import webkit
 
-class PyGTKSimpleBrowser:
+class SimpleBrowser:
     def __init__(self):
-        self.urlinput = gtk.Entry()
-        self.urlinput.connect("activate", self.loadurl)
+        builder = gtk.Builder()
+        builder.add_from_file("Interface.GtkBuilder")
+        builder.connect_signals(self)
 
-        self.gobutton = gtk.Button(stock = gtk.STOCK_APPLY)
-        self.gobutton.connect("clicked", self.loadurl)
+        self.window = builder.get_object("window")
+        self.addressbar = builder.get_object("addressbar")
+        self.refreshbutton = builder.get_object("refreshbutton")
+        self.stopbutton = builder.get_object("stopbutton")
+        self.tabs = builder.get_object("tabs")
 
-        self.refreshbutton = gtk.Button(stock = gtk.STOCK_REFRESH)
-        self.refreshbutton.connect("clicked", self.refresh)
+        self.tablist = []
 
-        self.stopbutton = gtk.Button(stock = gtk.STOCK_STOP)
-        self.stopbutton.connect("clicked", self.stoploading)
+    def get_current_tab(self):
+        return self.tablist[self.tabs.get_current_page()]
 
-        self.toolbar = gtk.HBox()
-        self.toolbar.pack_start(self.urlinput)
-        self.toolbar.pack_start(self.gobutton, False)
-        self.toolbar.pack_start(self.refreshbutton, False)
-        self.toolbar.pack_start(self.stopbutton, False)
-        
-        self.browser = webkit.WebView()
-        self.browser.connect("title-changed", self.changetitle)
-        self.browser.connect("load-progress-changed", self.changeprogress)
-        self.browser.connect("load-started", self.loadstarted)
-        self.browser.connect("load-finished", self.loadfinished)
+    def get_tab(self, page_num):
+        return self.tablist[page_num]
+
+    def get_url(self):            
+        return self.addressbar.get_text()
+
+    def loadurl(self):
+        url = self.get_url()
+        if not "://" in url:
+            url = "http://" + url
+
+        tab = self.get_current_tab()
+        tab.loadurl(url)
+
+    def on_addressbar_activate(self, widget):
+        self.loadurl()
+
+    def on_gobutton_clicked(self, button):
+        self.loadurl()
+
+    def on_refreshbutton_clicked(self, button):
+        tab = self.get_current_tab()
+        tab.refresh()
+
+    def on_stopbutton_clicked(self, button):
+        tab = self.get_current_tab()
+        tab.stoploading()
+
+    def on_tabaddbutton_clicked(self, button):
+        self.tab_add("about:blank")
+
+    def on_tabclosebutton_clicked(self, button):
+        self.tab_close()
+
+    def on_tabs_switch_page(self, notebook, page, page_num):
+        self.update_url_from_tab(page_num)
+        self.update_refreshbutton_from_tab(page_num)
+        self.update_stopbutton_from_tab(page_num)
+
+    def on_window_destroy(self, widget, data=None):
+        gtk.main_quit()
+
+    def set_progress(self, progress):
+        self.addressbar.set_progress_fraction(progress)
+
+    def set_title(self, tab, title):
+        self.tabs.set_tab_label_text(tab, title)
+
+    def set_url(self, url):
+        self.addressbar.set_text(url)
+
+    def show_refreshbutton(self, visibility):
+        self.refreshbutton.set_visible(visibility)
+
+    def show_stopbutton(self, visibility):
+        self.stopbutton.set_visible(visibility)
+
+    def tab_add(self, url):
+        newtab = SimpleTab(self, url)
+        self.tablist.append(newtab)
+
+        self.tabs.append_page(newtab.get_widget())
+        self.tabs.show_all()
+
+    def tab_close(self):
+        tab = self.tabs.get_current_page()
+        del self.tablist[tab]
+
+        self.tabs.remove_page(tab)
+
+    def update_stopbutton_from_tab(self, page_num):
+        tab = self.get_tab(page_num)
+        loadstatus = tab.get_loadstatus()
+
+        if loadstatus.value_nick != "finished" and loadstatus.value_nick != "failed":
+            self.show_stopbutton(True)
+        else:
+            self.show_stopbutton(False)
+
+    def update_refreshbutton_from_tab(self, page_num):
+        tab = self.get_tab(page_num)
+        loadstatus = tab.get_loadstatus()
+
+        if loadstatus.value_nick == "finished" or loadstatus.value_nick == "failed":
+            self.show_refreshbutton(True)
+        else:
+            self.show_refreshbutton(False)
+
+    def update_url_from_tab(self, page_num):
+        tab = self.get_tab(page_num)
+        url = tab.get_url()
+        self.set_url(url)
+
+    def main(self):
+        self.window.show_all()
+        self.tab_add("http://example.org")
+        self.show_stopbutton(False)
+
+        gtk.main()
+
+class SimpleTab:
+    def __init__(self, browser, url):
+        self.webview = webkit.WebView()
+        self.webview.connect("title-changed", self.changetitle)
+        self.webview.connect("load-progress-changed", self.changeprogress)
+        self.webview.connect("load-started", self.loadstarted)
+        self.webview.connect("load-finished", self.loadfinished)
+
+        self.webview.open(url)
 
         self.scroller = gtk.ScrolledWindow()
-        self.scroller.add(self.browser)
+        self.scroller.add(self.webview)
 
-        self.vbox = gtk.VBox()
-        self.vbox.pack_start(self.toolbar, False)
-        self.vbox.pack_start(self.scroller)
+        if not browser:
+            raise RuntimeError()
+        self.browser = browser
 
-        self.window = gtk.Window()
-        self.window.add(self.vbox)
-        self.window.connect("destroy", self.destroy)
-        self.window.set_title("A simple browser using PyGTK")
-
-        self.window.show_all()
-
-        self.stopbutton.set_visible(False)
+    def changetitle(self, webview, frame, title):
+        self.browser.set_title(self.get_widget(), title)
 
     def changeprogress(self, webview, amount):
         progress = amount / 100.0
-        self.urlinput.set_progress_fraction(progress)
+        self.browser.set_progress(progress)
 
-    def changetitle(self, webview, frame, title):
-        self.window.set_title(title)
-
-    def destroy(self, widget, data=None):
-        gtk.main_quit()
-
-    def loadurl(self, button):
-        url = self.urlinput.get_text()
-
-        if not "://" in url:
-            url = "http://" + url
-            self.urlinput.set_text(url)
-
-        self.browser.open(url)
-
-    def loadstarted(self, webview, frame):
-        self.refreshbutton.set_visible(False)
-        self.stopbutton.set_visible(True)
-
-        self.urlinput.set_progress_fraction(0)
+    def get_url(self):
+        return self.webview.get_property("uri")
 
     def loadfinished(self, webview, frame):
-        self.refreshbutton.set_visible(True)
-        self.stopbutton.set_visible(False)
-        self.urlinput.set_progress_fraction(0)
+        self.browser.show_refreshbutton(True)
+        self.browser.show_stopbutton(False)
+        self.browser.set_progress(0)
 
-    def refresh(self, button):
-        self.browser.reload()
+    def loadstarted(self, webview, frame):
+        self.browser.show_refreshbutton(False)
+        self.browser.show_stopbutton(True)
+        self.browser.set_progress(0)
 
-    def stoploading(self, button):
-        self.browser.stop_loading()
+    def loadurl(self, url):
+        self.webview.open(url)
 
-    def main(self):
-        gtk.main()
+    def refresh(self):
+        self.webview.reload()
+
+    def stoploading(self):
+        self.webview.stop_loading()
+
+    def get_loadstatus(self):
+        return self.webview.get_property("load-status")
+
+    def get_widget(self):
+        return self.scroller
 
 if __name__ == '__main__':
-    Browser = PyGTKSimpleBrowser()
+    Browser = SimpleBrowser()
     Browser.main()
